67188c49c11cb4055e18472c10d20fa9a05120c9
qkrwltjr993 <117136293+qkrwltjr993@users.noreply.github.com>
2023-01-09 02:54:55 +0900

    Update valiables.tf

--- VPC/valiables.tf
+++ VPC/valiables.tf
@@ -6,3 +6,15 @@ variable "tag_name" {
     description = "value"
     type = string
 }
+
+variable "vpc_id" {
+    description = "set vpc id"
+    type = string
+}
+
+variable "vpc_name" {
+    description = "set vpc name"
+    type = string
+}
+
+
