ea70689698c250c8fe4dd07074131436c0aa5653
qkrwltjr993 <117136293+qkrwltjr993@users.noreply.github.com>
2023-01-09 03:01:37 +0900

    Create outputs.tf

---
+++ tf-22shop-vpc-web/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_WEB_id" {
+  description = "The name of vpc WEB id"
+  value = aws_vpc.vpc-WEB.id
+}
+
+output "vpc_name" {
+  value = var.tag_name
+}
+
+output "subnet" {
+  description = "The name of vpc WEB id"
+  value = aws_subnet.subnets
+}
+
+output "igw_id" {
+  description = "The name of hq-WEB id"
+  value = aws_internet_gateway.WEB.id
+}
+
+output "route_public_id" {
+    description = "get route_public_id"
+    value = aws_route_table.public-table.id
+
+}
