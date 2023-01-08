63a52e0091393f77a1cda517543130cf7df19f3a
qkrwltjr993 <117136293+qkrwltjr993@users.noreply.github.com>
2023-01-09 03:09:10 +0900

    Create valiables.tf

---
+++ tf-22shop-vpc-pidc/valiables.tf
@@ -0,0 +1,18 @@
+variable "cidr_block" {
+    description = "value"
+    type = string
+}
+variable "tag_name" {
+    description = "value"
+    type = string
+}
+
+variable "vpc_id" {
+    description = "set vpc id"
+    type = string
+}
+
+variable "vpc_name" {
+    description = "set vpc name"
+    type = string
+}
