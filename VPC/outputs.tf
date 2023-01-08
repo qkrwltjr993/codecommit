25123f612254c06d349e119f871a71b56881015d
qkrwltjr993 <117136293+qkrwltjr993@users.noreply.github.com>
2023-01-09 02:52:52 +0900

    Update outputs.tf

--- VPC/outputs.tf
+++ VPC/outputs.tf
@@ -6,3 +6,19 @@ output "vpc_WEB_id" {
 output "vpc_name" {
   value = var.tag_name
 }
+
+output "subnet" {
+  description = "The name of vpc WEB id"
+  value = aws_subnet.subnets
+}
+
+output "igw_id" {
+  description = "The name of hq-WEB id"
+  value = aws_internet_gateway.WEB.id
+}
+
+output "route_public_id" {
+    description = "get route_public_id"
+    value = aws_route_table.public-table.id
+
+}
